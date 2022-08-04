2edcc3f21e71f95b65751a9f490053517382cc2c
Anhanh1412 <nguyenanh141297@gmail.com>
2022-08-05 00:16:24 +0700

    ok2

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ $('#loggin').click(() => {
         $('.mh2').show('3000')
         $('#pass').val("")
     } else {
-        $('.info').html('Nhập cái méo gì vậy???')
+        $('.info').html('Nhập cái méo gì vậy??? Ngày sinh Ny còn không nhớ ???')
         $('.toast').toast("show")
         $('#pass').val("")
     }
@@ -40,7 +40,7 @@ let a = 0
 $('.open').click(() => {
     $('.mh3').hide('3000')
     $('.mh4').removeClass('d-none')
-    $('.dk').html(`Thả ${b} tim để mở quà`)
+    $('.dk').html(`Thả tim để mở quà`)
     $('modal').show()
     $('.num').html(a)
 })
@@ -138,7 +138,7 @@ $('.like').click(() => {
     a++
     let d = Math.floor(Math.random() * 14)
     $('.num').html(a)
-    if (a == b) {
+    // if (a == b) {
         b = Math.floor(Math.random() * 13)+1
         a = 0
         $('.mh4').hide('3000')
@@ -153,7 +153,7 @@ $('.like').click(() => {
                         </div>
                     </div>
         `)
-    }
+    // }
 })
 
 $('.back').click(() => {
